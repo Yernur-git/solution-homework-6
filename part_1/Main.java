4100a64736658d066199f67bfbc0c25464934969
Yernur <erkeerke349@gmail.com>
2025-04-15 13:54:12 +0500

    Tech Support Request Handler demo

--- part_1/Main.java
+++ part_1/Main.java
@@ -1,4 +1,17 @@
 package part_1;
 
 public class Main {
+    public static void main(String[] args) {
+        SupportHandler faq = new FAQBotHandler();
+        SupportHandler junior = new JuniorSupportHandler();
+        SupportHandler senior = new SeniorSupportHandler();
+
+        faq.setNext(junior);
+        junior.setNext(senior);
+
+        String[] issues = {"password_reset", "refund_request", "account_ban", "unknown_bug"};
+        for (String issue : issues) {
+            faq.handle(issue);
+        }
+    }
 }
