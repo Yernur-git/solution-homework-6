8ef461f0deb55b123805f23a38ddfc087c99e0b7
Yernur <erkeerke349@gmail.com>
2025-04-15 13:35:09 +0500

    FAQBotHandler implementation

--- part_1/FAQBotHandler.java
+++ part_1/FAQBotHandler.java
@@ -1,4 +1,20 @@
 package part_1;
 
-public class FAQBotHandler {
+public class FAQBotHandler implements SupportHandler{
+    private SupportHandler nextHandler;
+
+    public void setNext(SupportHandler nextHandler) {
+        this.nextHandler = nextHandler;
+    }
+
+    @Override
+    public void handle(String issue) {
+        if(issue.equals("password_reset")){
+            System.out.println("[FAQBot] Handled " + issue);
+        } else if(nextHandler != null){
+            nextHandler.handle(issue);
+        } else {
+            System.out.println("[FAQBot] Cannot handle " + issue + " — escalate manually");
+        }
+    }
 }
