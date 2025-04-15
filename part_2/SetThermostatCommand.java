27967b4222e3b0bf6f3aa46cca3dfa98378b03b1
Yernur <erkeerke349@gmail.com>
2025-04-15 15:19:43 +0500

    concrete command class implementation

--- part_2/SetThermostatCommand.java
+++ part_2/SetThermostatCommand.java
@@ -1,4 +1,19 @@
 package part_2;
 
-public class SetThermostatCommand {
+public class SetThermostatCommand implements Command {
+    private Thermostat thermostat;
+    private int targetTemperature;
+
+    public SetThermostatCommand(Thermostat thermostat, int targetTemperature) {
+        this.thermostat = thermostat;
+        this.targetTemperature = targetTemperature;
+    }
+
+    public void execute() {
+        thermostat.setTemperature(targetTemperature);
+    }
+
+    public void undo() {
+        thermostat.revertTemperature();
+    }
 }
--- part_2/TurnOnLightCommand.java
+++ part_2/TurnOnLightCommand.java
@@ -1,4 +1,17 @@
 package part_2;
 
-public class TurnOnLightCommand {
+public class TurnOnLightCommand implements Command {
+    private Light light;
+
+    public TurnOnLightCommand(Light light) {
+        this.light = light;
+    }
+
+    public void execute() {
+        light.turnOn();
+    }
+
+    public void undo() {
+        light.turnOff();
+    }
 }
