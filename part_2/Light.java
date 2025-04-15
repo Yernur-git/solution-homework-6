e5f0038b8db74be1ab27f71c1a698dace902744a
Yernur <erkeerke349@gmail.com>
2025-04-15 14:12:16 +0500

    receivers implementation

--- part_2/Light.java
+++ part_2/Light.java
@@ -1,4 +1,19 @@
 package part_2;
 
 public class Light {
+    private boolean isOn;
+
+    public void turnOn(){
+        this.isOn = true;
+        System.out.println("[Light] Turning ON");
+    }
+
+    public void turnOff(){
+        this.isOn = false;
+        System.out.println("[Light] Turning OFF");
+    }
+
+    public boolean isOn() {
+        return isOn;
+    }
 }
--- part_2/Thermostat.java
+++ part_2/Thermostat.java
@@ -1,4 +1,14 @@
 package part_2;
 
 public class Thermostat {
+    private int currentTemperature;
+
+    public void setTemperature(int temperature) {
+        this.currentTemperature = temperature;
+        System.out.println("[Thermostat] Setting temperature to " + temperature + "°C");
+    }
+
+    public int getTemperature() {
+        return currentTemperature;
+    }
 }
