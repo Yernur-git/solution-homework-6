1763feaef8b2aaa8f87a1d0484f782beb2b54ccf
Yernur <erkeerke349@gmail.com>
2025-04-15 14:34:10 +0500

    thermostat fix

--- part_2/Thermostat.java
+++ part_2/Thermostat.java
@@ -2,13 +2,20 @@ package part_2;
 
 public class Thermostat {
     private int currentTemperature;
+    private int previousTemperature;
 
     public void setTemperature(int temperature) {
-        this.currentTemperature = temperature;
+        previousTemperature = currentTemperature;
+        currentTemperature = temperature;
         System.out.println("[Thermostat] Setting temperature to " + temperature + "°C");
     }
 
     public int getTemperature() {
         return currentTemperature;
+    }
+
+    public void revertTemperature() {
+        System.out.println("[Thermostat] Reverting to previous temperature");
+        currentTemperature = previousTemperature;
     }
 }
