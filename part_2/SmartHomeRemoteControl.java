a8f15393894d8f0f94b983cb606b801d6c8af9f2
Yernur <erkeerke349@gmail.com>
2025-04-15 15:30:36 +0500

    Invoker implementation

--- part_2/SmartHomeRemoteControl.java
+++ part_2/SmartHomeRemoteControl.java
@@ -1,4 +1,34 @@
 package part_2;
 
+import java.util.Stack;
+
 public class SmartHomeRemoteControl {
+    private Command[] slots;
+    private Stack<Command> commandHistory = new Stack<>();
+
+    public SmartHomeRemoteControl(int numSlots) {
+        slots = new Command[numSlots];
+    }
+
+    public void setCommand(int slot, Command command) {
+        if (slot >= 0 && slot < slots.length) {
+            slots[slot] = command;
+        }
+    }
+
+    public void pressButton(int slot) {
+        if (slot >= 0 && slot < slots.length && slots[slot] != null) {
+            Command command = slots[slot];
+            command.execute();
+            commandHistory.push(command);
+        }
+    }
+
+    public void undo() {
+        if (!commandHistory.isEmpty()) {
+            System.out.println("Undo last command");
+            Command Command = commandHistory.pop();
+            Command.undo();
+        }
+    }
 }
