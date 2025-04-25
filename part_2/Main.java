bc1b6bc8826e2b897df1d361ede450c54107ce58
Yernur Bekbolat <erkeerke349@gmail.com>
2025-04-25 17:07:45 +0500

    Smart Home Remote Control demo
    i accidentally deleted this commit

--- part_2/Main.java
+++ part_2/Main.java
@@ -1,4 +1,21 @@
 package part_2;
 
 public class Main {
+    public static void main(String[] args) {
+        Light livingRoomLight = new Light();
+        Thermostat thermostat = new Thermostat();
+
+        Command lightOnCommand = new TurnOnLightCommand(livingRoomLight);
+        Command setThermostatCommand = new SetThermostatCommand(thermostat, 22);
+
+        SmartHomeRemoteControl remote = new SmartHomeRemoteControl(5);
+
+        remote.setCommand(0, lightOnCommand);
+        remote.setCommand(1, setThermostatCommand);
+
+        remote.pressButton(0);
+        remote.pressButton(1);
+    
+        remote.undo();
+    }
 }
